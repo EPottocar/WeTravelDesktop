156f8e169902ad3b091b674aba038e5f1c7de74d
edoardopottocar <edoardo.pottocar@gmail.com>
2021-08-19 11:15:33 +0200

    MainFrame

--- src/com/company/GUI/MainFrame.java
+++ src/com/company/GUI/MainFrame.java
@@ -11,9 +11,10 @@ public class MainFrame extends JFrame {
     private JPanel MainFrame;
     private JButton homeButton;
     private JButton impostazioniButton;
-    private JTextField BENVENUTOINWETRAVELTextField;
+    private JTextField WETRAVELTextField;
     private JPanel Frame;
     private JButton Città;
+    private JTextArea Newsletter;
 
 
     public MainFrame() {
@@ -21,7 +22,7 @@ public class MainFrame extends JFrame {
         setContentPane(MainFrame);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setVisible(true);
-        setSize(1500,800);
+        setSize(1500, 800);
 
 
         homeButton.addActionListener(new ActionListener() {
